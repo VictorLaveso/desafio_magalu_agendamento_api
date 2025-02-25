b3ee0fd9a1ab4700d3d65d42a6418d3bd64b4e02
VictorLaveso <victorhugolavezo@gmail.com>
2025-02-25 11:27:56 -0300

    adicionado metodos de busca de notificacao

--- src/main/java/com/magalu/desafio/business/AgendamentoService.java
+++ src/main/java/com/magalu/desafio/business/AgendamentoService.java
@@ -4,6 +4,7 @@ package com.magalu.desafio.business;
 import com.magalu.desafio.business.mapper.IAgendamentoMapper;
 import com.magalu.desafio.controller.dto.in.AgendamentoRecord;
 import com.magalu.desafio.controller.dto.out.AgendamentoRecordOut;
+import com.magalu.desafio.core.NotFoundException;
 import com.magalu.desafio.core.repositories.AgendamentoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -15,10 +16,15 @@ public class AgendamentoService {
 
     private final AgendamentoRepository repository;
     private final IAgendamentoMapper agendamentoMapper;
+    private final AgendamentoRepository agendamentoRepository;
 
     public AgendamentoRecordOut gravarAgendamento(AgendamentoRecord agendamento) {
         return agendamentoMapper.paraout(
                 repository.save(
                         agendamentoMapper.paraEntity(agendamento)));
-    };
+    }
+
+    public AgendamentoRecordOut buscarAgendamentoPorId(Long id) {
+        return agendamentoMapper.paraout(repository.findById(id).orElseThrow(() -> new NotFoundException("Id não encontrado")));
+    }
 }
