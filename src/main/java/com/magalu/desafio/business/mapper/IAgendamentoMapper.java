3ab5aafe758f4484575fed38e3a8d26d3ff0ae22
VictorLaveso <victorhugolavezo@gmail.com>
2025-02-26 11:33:54 -0300

    adicionado metodo de cancelamento de agendamento

--- src/main/java/com/magalu/desafio/business/mapper/IAgendamentoMapper.java
+++ src/main/java/com/magalu/desafio/business/mapper/IAgendamentoMapper.java
@@ -4,8 +4,10 @@ import com.magalu.desafio.controller.dto.in.AgendamentoRecord;
 import com.magalu.desafio.controller.dto.out.AgendamentoRecordOut;
 import com.magalu.desafio.core.entities.Agendamento;
 import org.mapstruct.Mapper;
-import org.springframework.context.annotation.Bean;
-
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import java.time.LocalDateTime;
+import com.magalu.desafio.core.enums.StatusNotificacaoEnum;
 import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
 
 @Mapper(componentModel = SPRING)
@@ -15,4 +17,8 @@ public interface IAgendamentoMapper {
     Agendamento paraEntity(AgendamentoRecord agendamento);
 
     AgendamentoRecordOut paraout(Agendamento agendamento);
+
+    @Mapping(target = "dataHoraModificacao", expression = "java(LocalDateTime.now())")
+    @Mapping(target = "statusNotificacaoEnum", expression = "java(StatusNotificacaoEnum.CANCELADO)")
+    Agendamento paraEntityCancelamento( Agendamento agendamento);
 }
